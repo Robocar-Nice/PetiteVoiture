c24bb543af17c2e3e1d26285925217c42ab91afa
Eric Faure Dunand Bruschi <eric.faure-dunand-bruschi@epitech.eu>
2026-09-18 17:08:00 +0200

    [+] added real code

---
+++ sketch_sep15a/real.ino
@@ -0,0 +1,93 @@
+// Moteur A
+const int ENA = 9;   // PWM - vitesse moteur A
+const int IN1 = 3;
+const int IN2 = 4;
+
+// Moteur B
+const int ENB = 10;  // PWM - vitesse moteur B
+const int IN3 = 5;
+const int IN4 = 6;
+
+void setup() {
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char val = Serial.read();
+
+    Serial.print(val);
+
+    if (val == 'F') {
+      avancer(200);
+    }
+    if (val == 'B') {
+      avancer(200);
+    }
+    if (val == 'R') {
+      avancer(200);
+    }
+    if (val == 'L') {
+      avancer(200);
+    }
+  }
+}
+
+void avancer(int vitesse) {
+  // Moteur A avant
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  // Moteur B avant
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  analogWrite(ENA, vitesse);
+  analogWrite(ENB, vitesse);
+}
+
+void reculer(int vitesse) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  analogWrite(ENA, vitesse);
+  analogWrite(ENB, vitesse);
+}
+
+void tournerDroite(int vitesse) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  analogWrite(ENA, vitesse);
+  analogWrite(ENB, vitesse);
+}
+
+void tournerGauche(int vitesse) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  analogWrite(ENA, vitesse);
+  analogWrite(ENB, vitesse);
+}
+
+void arreter() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+}
